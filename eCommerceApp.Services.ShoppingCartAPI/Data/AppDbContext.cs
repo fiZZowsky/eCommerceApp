8a612fa2fd49af15e61014cb9d2a7912e059f09e
Piotr Równicki <piotr.rownicki@gearcode.eu>
2024-08-27 15:42:45 +0200

    Added shopping cart project to solution

---
+++ eCommerceApp.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using eCommerceApp.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceApp.Services.ShoppingCartAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+    }
+}
---
+++ eCommerceApp.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using eCommerceApp.Services.ShoppingCartAPI.Models;
+using eCommerceApp.Services.ShoppingCartAPI.Models.Dto;
+
+namespace eCommerceApp.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CartHeaderDto, CartHeader>().ReverseMap();
+                config.CreateMap<CartDetailsDto, CartDetails>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
