b1b05cb2fade9d4a8bc245cd307208109218c184
Piotr Równicki <piotr.rownicki@gearcode.eu>
2024-08-27 15:14:01 +0200

    Added product list to main page and product details

--- eCommerceApp.Web/Models/ProductDto.cs
+++ eCommerceApp.Web/Models/ProductDto.cs
@@ -1,4 +1,6 @@
-﻿namespace eCommerceApp.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceApp.Web.Models
 {
     public class ProductDto
     {
@@ -8,5 +10,7 @@
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
+        [Range(0, 100)]
+        public int Count { get; set; } = 1;
     }
 }
