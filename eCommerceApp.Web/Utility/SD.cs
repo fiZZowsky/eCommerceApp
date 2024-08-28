6e5eb9b200ca088d82eba687d674f897e4ea0f23
Piotr Równicki <piotr.rownicki@gearcode.eu>
2024-08-28 10:37:44 +0200

    Added services for cart in frontend

--- eCommerceApp.Web/Utility/SD.cs
+++ eCommerceApp.Web/Utility/SD.cs
@@ -5,9 +5,12 @@
         public static string CouponAPIBase { get; set; }
         public static string ProductAPIBase { get; set; }
         public static string AuthAPIBase { get; set; }
+        public static string ShoppingCartAPIBase { get; set; }
+
         public const string RoleAdmin = "ADMIN";
         public const string RoleCustomer = "CUSTOMER";
         public const string TokenCookie = "JTWToken";
+
         public enum ApiType
         {
             GET,
